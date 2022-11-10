f89ccce54bf815d9c0750a6f1419ce15555e7000
rozhiar <rozyar.313175000@uhd.edu.iq>
2022-11-10 16:20:16 +0300

    Completed assigment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
-  
-}
+  const form = document.querySelector('form');
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const input = document.querySelector('input#searchByID');
+        fetch(`http://localhost:3000/movies`)
+        .then(response => response.json()).then((data) => {
+            data.forEach(movie => {
+                if (movie.id == input.value) {
+                    const title = document.querySelector('section#movieDetails h4');
+                    const summary = document.querySelector('section#movieDetails p');
+                    title.innerText = movie.title;
+                    summary.innerText = movie.summary;
+                }
+
+            });
+
+        });
+});
+};
 
 document.addEventListener('DOMContentLoaded', init);
+
